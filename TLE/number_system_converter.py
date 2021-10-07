4545c6c5f02da9435d064737fd30a11483cf9877
LuanBog <mynameiscutepanda@gmail.com>
2021-10-07 12:36:55 +0800

    Added octal and hexadecimal converter

--- TLE/number_system_converter.py
+++ TLE/number_system_converter.py
@@ -2,7 +2,7 @@
 
 import math
 
-def to_binary(decimal, show_solution_only=False):
+def to_binary(decimal):
     binary = ''
     target = decimal
 
@@ -10,29 +10,83 @@ def to_binary(decimal, show_solution_only=False):
         result = target / 2
         reminder = '0' if float(result).is_integer() else '1'
 
-        if show_solution_only:
-            print('{}\u2081\u2080 / 2 = {} reminder. {}'.format(target, math.trunc(result) if float(result).is_integer() else result, reminder))
+        print('{}\u2081\u2080 / 2 = {} reminder. {}'.format(target, math.trunc(result) if float(result).is_integer() else result, reminder))
         
         binary += reminder
 
         target = math.trunc(result)
 
-    if not show_solution_only:
-        return binary[::-1]
-    else:
-        return None
+    return binary[::-1]
+
+def to_octal(decimal):
+    octal = ''
+    target = decimal
+
+    while target > 0:
+        result = target / 8
+        reminder = float('0.' + str(result).split('.')[1]) * 8
+
+        print('{}\u2081\u2080 / 8 = {} reminder. {}'.format(target, math.trunc(result) if float(result).is_integer() else result, math.trunc(reminder)))
+        
+        octal += str(math.trunc(reminder))
+
+        target = math.trunc(result)
+
+    return octal[::-1]
+
+def to_hexadecimal(decimal):
+    hexadecimal = []
+    target = decimal
+
+    letters = {'10': 'a', '11': 'b', '12': 'c', '13': 'd', '14': 'e', '15': 'f', '16': 'g'}
+
+    while target > 0:
+        result = target / 16
+        reminder = float('0.' + str(result).split('.')[1]) * 16
+
+        print('{}\u2081\u2080 / 16 = {} reminder. {}'.format(target, math.trunc(result) if float(result).is_integer() else result, math.trunc(reminder)))
+        
+        hexadecimal.append(str(math.trunc(reminder)))
+
+        target = math.trunc(result) 
+
+    for index, byte in enumerate(hexadecimal):
+        if byte in letters.keys():
+            letter = letters[byte]
+
+            print('{} -> {}'.format(byte, letter))
+            hexadecimal[index] = letter
+
+    return ''.join(hexadecimal)[::-1]
+
+def get_sub_number(to):
+    if to == 'a':
+        return '\u2082'
+    elif to == 'b':
+        return '\u2088'
+    elif to == 'c':
+        return '\u2081\u2086'
 
 def main():
-    # print('Convert from:')
-    # from_ = input('[A] Decimal (Base 10) [More coming soon]: ')
-    # print('Convert to:')
-    # to = input('[A] Binary (Base 2): ')
-    decimal = int(input('Decimal value: '))
+    print('Convert from:')
+    from_ = input('[A] Decimal (Base 10) [More coming soon]: ')
+    print('Convert to:')
+    to = input('[A] Binary (Base 2), [B] Octal (Base 8), [C] Hexadecimal (Base 16): ').lower()
+    decimal = int(input('Value: '))
 
+    result = ''
+    sub_number = get_sub_number(to)
+    
     print('\n---------------------------------------- RESULT ----------------------------------------\n')
-    to_binary(decimal, show_solution_only=True)
-    result = to_binary(decimal)
-    print('{}\u2081\u2080 = {}\u2082'.format(decimal, result))
+
+    if to == 'a':
+        result = to_binary(decimal)
+    elif to == 'b':
+        result = to_octal(decimal)
+    elif to == 'c':
+        result = to_hexadecimal(decimal)
+
+    print('\n{}\u2081\u2080 = {}{}'.format(decimal, result, sub_number))
 
 if __name__ == '__main__':
     main()
